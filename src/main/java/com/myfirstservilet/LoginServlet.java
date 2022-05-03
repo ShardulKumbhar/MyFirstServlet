be80cc34490b70c86d77c8bdd07a294b3d10f098
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-05-04 00:20:33 +0530

    Added validation to user name

--- src/main/java/com/myfirstservilet/LoginServlet.java
+++ src/main/java/com/myfirstservilet/LoginServlet.java
@@ -29,9 +29,21 @@ public class LoginServlet extends HttpServlet {
         //get servlet config init params
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-
+        //regex for username and password
+        String userRegex = "^[A-Z]{1}.{2,}$";//username regex
+        /*
+        Checking for
+        1.User name regex
+        2. Atleast one upper case
+        3. Min 3 characters
+         */
+        if (!user.matches(userRegex)) {
+            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Incorrect User , Enter the Correct user name</font>");
+            requestDispatcher.include(request, response);
+        }
         if (userID.equals(user) && password.equals(pwd)) {
-
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
